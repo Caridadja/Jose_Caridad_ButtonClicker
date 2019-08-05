ffc5cd9d421159dabc61e33369999d1fa06b575c
Caridadja <50273940+Caridadja@users.noreply.github.com>
2019-08-05 17:07:42 -0500

    Add files via upload

---
+++ Counter/src/com/caridadja/web/controllers/Counter.java
@@ -0,0 +1,57 @@
+package com.caridadja.web.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Counter
+ */
+@WebServlet("/Counter")
+public class Counter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Counter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		String tracker = (String) session.getAttribute("tracker");
+		Integer counter = 0;
+		if(tracker == null) {
+			session.setAttribute("tracker", counter.toString());
+		}
+		else {
+			counter = Integer.parseInt(tracker);
+			counter += 1; 
+			tracker = counter.toString();
+			session.setAttribute("tracker", tracker);
+		}
+		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/counter.jsp");
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
